cbb25fb0db145f8fe608fd922419c4b458d4ffa7
kares <self@kares.org>
2016-03-17 08:47:07 +0100

    initial support for deep to_rb conversion
    ... a.k.a. unmirror-ing in terms of nashorn

--- lib/nashorn/convert.rb
+++ lib/nashorn/convert.rb
@@ -1,11 +1,12 @@
 class << Nashorn
 
-  def to_rb(object)
-    return nil if Nashorn::JS::ScriptObjectMirror.isUndefined(object)
+  def to_rb(object, unmirror = false)
     # ConsString for optimized String + operations :
     return object.toString if object.is_a?(Java::JavaLang::CharSequence)
-    return object.unwrap if object.is_a?(Nashorn::Ruby::Object)
-    return object.unwrap if object.is_a?(Nashorn::Ruby::Function)
+    return object.unwrap if object.is_a?(Nashorn::RubyObject)
+    return object.unwrap if object.is_a?(Nashorn::RubyFunction)
+    return nil if Nashorn::JS::ScriptObjectMirror.isUndefined(object)
+    return js_mirror_to_rb(object) if unmirror
     object
   end
   alias_method :to_ruby, :to_rb
@@ -18,7 +19,7 @@ class << Nashorn
     when Nashorn::JS::JSObject then object
     #when Array                 then array_to_js(object)
     #when Hash                  then hash_to_js(object)
-    when Time                  then time_to_js(object)
+    when Time                  then object.to_java
     when Method, UnboundMethod then Nashorn::Ruby::Function.wrap(object)
     when Proc                  then Nashorn::Ruby::Function.wrap(object)
     when Class                 then Nashorn::Ruby::Constructor.wrap(object)
@@ -35,16 +36,18 @@ class << Nashorn
     args.map { |arg| to_js(arg) }.to_java
   end
 
-  private
-
-  #def array_to_js(rb_array)
-  #end
-
-  #def hash_to_js(rb_hash)
-  #end
-
-  def time_to_js(time)
-    time.to_java
+  def js_mirror_to_rb(object, deep = true)
+    object = Nashorn::JS::ScriptUtils.unwrap(object)
+    if object.is_a?(Nashorn::JS::JSObject)
+      return object.values.to_a if object.isArray
+      return object if object.isFunction
+      # Nashorn::JS::ScriptObjectMirror is already a Map but still :
+      hash = {}
+      object.keySet.each { |key| hash[key] = to_rb object[key], true }
+      hash
+    else
+      object
+    end
   end
 
 end
